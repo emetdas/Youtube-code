c115eca046af3702342f24e0d3957798bf95a400
emetdas <emet@b2bleadstaff.com>
2022-01-27 19:29:31 +0600

    new animated tabs coding is done

---
+++ Animated tab/script.js
@@ -0,0 +1,26 @@
+let filter_btn = document.querySelectorAll('.filter-btn');
+let tab_items = document.querySelectorAll('.tab-item');
+
+for (let i = 0; i < filter_btn.length; i++) {
+  filter_btn[i].addEventListener('click', function () {
+    for (let j = 0; j < filter_btn.length; j++) {
+      filter_btn[j].classList.remove('active');
+    }
+    let select_tab = filter_btn[i].getAttribute('data-tab');
+    filter_btn[i].classList.add('active');
+    for (let t = 0; t < tab_items.length; t++) {
+      document.querySelector('.tab-filter-item-container').style.height =
+        tab_items[t].scrollHeight + 'px';
+      if (tab_items[t].classList.contains(select_tab)) {
+        tab_items[t].classList.add('select_tab');
+      } else {
+        tab_items[t].classList.remove('select_tab');
+      }
+    }
+  });
+}
+
+for (let th = 0; th < tab_items.length; th++) {
+  document.querySelector('.tab-filter-item-container').style.height =
+    tab_items[th].scrollHeight + 'px';
+}
