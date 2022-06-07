3914fed93955f9078c81e6e9e2659426c04013f6
emetdas <emet@b2bleadstaff.com>
2022-06-07 22:20:52 +0600

    hero design serice is coming

---
+++ website/Hero-Design/Banking/script.js
@@ -0,0 +1,6 @@
+let menu_toggle_icon = document.querySelector('.mobile_menu_toggle');
+let nav_menu = document.querySelector('.nav_menu');
+
+menu_toggle_icon.addEventListener('click', () => {
+  nav_menu.classList.toggle('active_menu');
+});
