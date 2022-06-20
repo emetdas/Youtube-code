39bb3054523f58bacffa8b1cc01a6436141b7250
emetdas <emet@b2bleadstaff.com>
2022-06-20 19:35:57 +0600

    new project is coming

---
+++ website/Hero-Design/Organic/data.md
@@ -0,0 +1,69 @@
+## Swiper Js & Web Icon
+```html
+    <link rel="stylesheet" href="https://unpkg.com/swiper@8/swiper-bundle.min.css" />
+
+    <script src="https://unpkg.com/swiper@8/swiper-bundle.min.js"></script>
+    <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+```
+## Css Reset
+```css
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap');
+*,
+*::after,
+*::before {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  text-decoration: none;
+  list-style-type: none;
+  outline: none;
+}
+
+:root {
+  --container: 144rem;
+  --m-auto: 0 auto;
+  --white: #fff;
+  --text-dark: #22242A;
+  --text-light: #7B7E86;
+  --btn-bg: rgb(240, 139, 24);
+  --slide-bg: linear-gradient(90deg, #FFEFBA 0%, #FFFFFF 100%);
+  --border: #E5E7EB;
+  --btn-border: #FFA751;
+  --scrollbar-bg: rgb(68, 75, 133);
+  --transition: all 0.5s ease-in-out;
+}
+
+html {
+  font-family: 'Roboto', sans-serif;
+  font-size: 62.5%;
+  scroll-behavior: smooth;
+}
+
+body {
+  font-size: 1.6rem;
+}
+
+::-webkit-scrollbar {
+  width: 1.2rem;
+}
+
+::-webkit-scrollbar-track {
+  background: var(--border);
+}
+
+::-webkit-scrollbar-thumb {
+  background: var(--scrollbar-bg);
+  border-radius: 2rem;
+}
+
+.container {
+  max-width: var(--container);
+  margin: 0 auto;
+}
+```
+Fresh and organic
+
+Fresh vegetables without pesticides to your table directly <br> from the garden
+
+Buy Now
---
+++ website/Hero-Design/Organic/script.js
@@ -0,0 +1,52 @@
+let nav_links = document.querySelectorAll('.nav_link');
+let nav_menu = document.querySelector('.nav_menu');
+let hilight_dot = document.querySelector('.hilight-dot');
+let mobile_menu_toggle_icon = document.querySelector('.mobile_menu_toggle_icon');
+let toggle_icon = mobile_menu_toggle_icon.querySelector('ion-icon');
+
+mobile_menu_toggle_icon.addEventListener('click', () => {
+  nav_menu.classList.toggle('active');
+  if (nav_menu.classList.contains('acitve')) {
+    toggle_icon.setAttribute('name', 'close-outline');
+  } else {
+    toggle_icon.setAttribute('name', 'menu-outline');
+
+  }
+});
+
+
+
+// Active Link
+let hilight = nav_links[0].getBoundingClientRect();
+let menu_rect = nav_menu.getBoundingClientRect();
+
+let coords = {
+  top: hilight.top - menu_rect.top + 22,
+  left: hilight.left - menu_rect.left + hilight.width / 2,
+};
+hilight_dot.style.transform = `translate(${coords.left}px,${coords.top}px)`;
+nav_links.forEach((link) => {
+  link.addEventListener('click', (e) => {
+    e.preventDefault();
+    let dot = link.getBoundingClientRect();
+    let menu_rect = nav_menu.getBoundingClientRect();
+    let coords = {
+      top: dot.top - menu_rect.top + 22,
+      left: dot.left - menu_rect.left + dot.width / 2,
+    };
+    hilight_dot.style.transform = `translate(${coords.left}px,${coords.top}px)`;
+  });
+});
+// Active Link
+
+// Slider
+const swiper = new Swiper('.swiper', {
+  sliderPerView: 1,
+  spaceBetween: 10,
+  grabCursor: true,
+  loop: true,
+  navigation: {
+    nextEl: '.slider-next',
+    prevEl: '.slider-prev',
+  },
+});
