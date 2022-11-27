0f019a7b7c5f660b9afd48c132b831eeef21726b
emetdas <emetdas552@gmail.com>
2022-11-27 10:45:39 +0600

    update is done

--- website/fifa-world-cup-Hero-Design/script.js
+++ website/fifa-world-cup-Hero-Design/script.js
@@ -37,3 +37,68 @@ document.addEventListener('DOMContentLoaded', () => {
     time();
     // count-down-end
 });
+
+async function fetchPoints() {
+    let points_wrapper = document.querySelector('.points-container');
+    let loader = document.querySelector('.loader');
+    let url = 'https://world-cup.codsfli.com/points.php';
+    let data = await fetch(url);
+    if (data.ok) {
+        setTimeout(async() => {
+                    loader.remove();
+                    let response = await data.json();
+                    response.map((groups) => {
+                                let sor = groups.teams.sort((a, b) => {
+                                    return a.position - b.position;
+                                });
+                                points_wrapper.innerHTML += `
+                <div class="points-table">
+  <h1 class="group-heading">${groups.group}</h1>
+  <table>
+    <thead>
+      <tr>
+        <th>Team</th>
+        <th>MP</th>
+        <th>L</th>
+        <th>D</th>
+        <th>W</th>
+        <th>Pts</th>
+      </tr>
+    </thead>
+    <tbody>
+      ${sor
+        .map(
+          (team) => `
+      <tr>
+        <td>
+          <div class="d-a">
+            <img
+              src="${team.flag}"
+              alt="${team.Team}"
+              class="team-flag"
+            />
+            <span>${team.flag
+              .split('https://world-cup.codsfli.com/flag/')
+              .join('')
+              .split('.png')
+              .join('')}</span>
+          </div>
+        </td>
+        <td>${team.match_play}</td>
+        <td>${team.loss}</td>
+        <td>${team.draw}</td>
+        <td>${team.win}</td>
+        <td>${team.points}</td>
+      </tr>
+      `
+        )
+        .join('')}
+    </tbody>
+  </table>
+</div>
+                `;
+      });
+    }, 1000);
+  }
+}
+fetchPoints();
